c0eb467fa3e58d8f6158d270ee616cebfa359c61
wanderleysoliveira <wanderley_santos@ymail.com>
2022-06-27 21:44:10 -0300

    inclusão de validação de campos e tratativa expecifica de exceção.

--- src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
@@ -2,7 +2,12 @@ package academy.devdojo.springboot2.requests;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class AnimePostRequestBody {
+    @NotEmpty
+    @NotNull
     private String name;
 }
